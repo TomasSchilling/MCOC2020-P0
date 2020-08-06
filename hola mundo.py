c5baa1ddc642c58cac7f9222048f5f676eb92805
TomasSchilling <69157271+TomasSchilling@users.noreply.github.com>
2020-08-05 23:16:04 -0400

    Add files via upload

---
+++ hola mundo.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 10:53:54 2020
+
+@author: Arkantos
+"""
+
+nombre= "tomas"
+mundo="mundo"
+
+
+print ("hola mundo soy tomas")
+
+print ("hola",mundo,"soy",nombre)
+
+print ("hola "+mundo+" soy "+nombre)
+
+print( "hola",(x for x in mundo),"soy",(x for x in nombre))
+
